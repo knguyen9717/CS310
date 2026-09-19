25ef77ff04848ef7e98ebe8020fff187bc0209b9
Khanh Nguyen <kn9717@gmail.com>
2026-09-19 08:55:59 -0500

    Add corrected Week 3 discussion solution for Harry Laack

---
+++ Module 3/ClassmateReply_HLaack.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+// Harry Laack
+// Week 3 Discussion Post - corrected solution
+
+int main() {
+    std::cout << "Welcome to the Week 3 Discussion Post!" << std::endl;
+    std::cout << "There are two errors about. Can you find them?" << std::endl;
+
+    bool isValid = false;
+
+    if (!isValid) {
+        std::cout << "Please enter a valid input." << std::endl;
+    }
+    else if (isValid) {
+        std::cout << "Please enter a valid input." << std::endl;
+    }
+    else {
+        std::cout << "You've entered a valid input." << std::endl;
+    }
+
+    int num_of_hours = 13;
+    int num_of_minutes = 0;
+
+    switch (num_of_hours) {
+        case 12:
+            std::cout << "It is noon." << std::endl;
+            num_of_minutes = 720;
+            break;
+        case 13:
+            std::cout << "It is 1:00pm" << std::endl;
+            num_of_minutes = 780;
+            break;
+    }
+
+    return 0;
+}
